36d4a81d57c087cdc0f566daa442656e2bd941e7
anmoltiwari13 <tiwarianmol2003@gmail.com>
2026-04-02 17:17:10 +0530

    Fix Render startup working directory

--- backend/start.sh
+++ backend/start.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+cd /app
+
 if [ -n "${RENDER_EXTERNAL_URL}" ]; then
   export FRONTEND_BASE_URL="${FRONTEND_BASE_URL:-$RENDER_EXTERNAL_URL}"
   export CORS_ORIGINS="${CORS_ORIGINS:-$RENDER_EXTERNAL_URL}"
